e81a7e2a18ded6191225619815fff2b11b57d1e6
RealebogaMagopa <61046703+RealebogaMagopa@users.noreply.github.com>
2020-11-17 21:14:35 +0200

    Changes follow after last changes

---
+++ WebApplication11/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication11.Controllers
+{
+    
+
+    
+    public class UserController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public WebApplication11(IConfiguration config)
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
